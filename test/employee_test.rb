99b2116c46a84cfb66e6f1156b0db6c6f4827b69
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-17 10:05:46 -0600

    Deleted earlier attempts to load CSV (not committed)

--- test/employee_test.rb
+++ test/employee_test.rb
@@ -12,19 +12,8 @@ class EmployeeTest < Minitest::Test
     @employee = Employee.new(employee_id, name, role, start_date, end_date)
   end
 
-  def test_instantiation
-    employee_id = '5'
-    name = 'Sally Jackson'
-    role = 'Engineer'
-    start_date = '2015-01-01'
-    end_date = '2018-01-01'
-
-    employee = Employee.new(employee_id, name, role, start_date, end_date)
-    dh = DateHandler::DHDate(my_date)
-
+  def test_employee_exists
     assert_instance_of Employee, employee
-    assert_equal 5, employee.employee_id
-    assert_instance_of Date, employee.start_date
   end
 
   def test_attributes
