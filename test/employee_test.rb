7865ffadd26da6e8f0fb563c18f7597c2d9e4e0b
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-17 10:04:26 -0600

    Added setup for employee.

--- test/employee_test.rb
+++ test/employee_test.rb
@@ -3,6 +3,15 @@ require './lib/employee'
 
 class EmployeeTest < Minitest::Test
 
+  def setup
+    employee_id = '5'
+    name = 'Sally Jackson'
+    role = 'Engineer'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    @employee = Employee.new(employee_id, name, role, start_date, end_date)
+  end
+
   def test_instantiation
     employee_id = '5'
     name = 'Sally Jackson'
@@ -12,7 +21,7 @@ class EmployeeTest < Minitest::Test
 
     employee = Employee.new(employee_id, name, role, start_date, end_date)
     dh = DateHandler::DHDate(my_date)
-    
+
     assert_instance_of Employee, employee
     assert_equal 5, employee.employee_id
     assert_instance_of Date, employee.start_date
