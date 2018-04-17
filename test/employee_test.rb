be6fa01a1c622f81c91f154ebe8b3f751e8b1471
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-17 09:33:20 -0600

    Initial commit

--- test/employee_test.rb
+++ test/employee_test.rb
@@ -3,6 +3,8 @@ require './lib/employee'
 
 class EmployeeTest < Minitest::Test
   def test_instantiation
+    employee = Employee.new('./data/employees.csv')
+    assert_instance_of Employee, 
   end
 
   def test_attributes
