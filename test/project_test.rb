dbed8fdc861b7284643001a5a0b4b647d439ee60
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-17 10:50:38 -0600

    Initial tests for timesheet class created.

---
+++ test/project_test.rb
@@ -0,0 +1,30 @@
+require './test/test_helper'
+require './lib/project'
+
+class ProjectTest < Minitest::Test
+
+  def setup
+    project_id = '123'
+    name = 'Widget Maker'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    @project = Project.new('123', 'Widget Maker', '2015-01-01', '2018-01-01')
+  end
+
+  def test_project_exists
+    assert_instance_of Project, @project
+  end
+
+  def test_project_has_attributes
+
+    assert_equal 123, @project.project_id
+    assert_equal Integer, @project.project_id.class
+    assert_equal 'Widget Maker', @project.name
+    assert_equal Date.new(2015, 1, 1), @project.start_date
+    assert_equal Date.new(2018, 1, 1), @project.end_date
+  end
+
+
+
+
+end
---
+++ test/timesheet_test.rb
@@ -0,0 +1,29 @@
+require './test/test_helper'
+require './lib/timesheet'
+
+class TimesheetTest < Minitest::Test
+
+  def setup
+    employee_id = '5'
+    project_id = '7'
+    date = '2015-01-01'
+    minutes = '120'
+    @timesheet = Timesheet.new(employee_id, project_id, date, minutes)
+  end
+
+  def test_timesheet_exists
+    assert_instance_of Timesheet, @timesheet
+  end
+
+  def test_timesheet_has_attributes
+    assert_equal 5, @timesheet.employee_id
+    assert_instance_of Integer, @timesheet.employee_id.class
+    assert_equal 7, @timesheet.project_id
+    assert_instance_of Integer, @timesheet.project_id.class
+    assert_equal Date.new(2015, 1, 1), timesheet.date
+    assert_equal 120, timesheet.minutes
+  end
+
+
+
+end
