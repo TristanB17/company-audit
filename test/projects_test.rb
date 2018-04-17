9c8272cd27c6ec29ddd2529d37dee13ed8fbe088
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-17 10:27:03 -0600

    Added project attribute test.

--- test/projects_test.rb
+++ test/projects_test.rb
@@ -14,8 +14,15 @@ class EmployeeTest < Minitest::Test
 
   def test_project_exists
     assert_instance_of Project, project
-  end 
+  end
 
+  def test_project_has_attributes
+    assert_equal 123, project.project_id
+    assert_equal Integer, project.project_id.class
+    assert_equal 'Widget Maker', project.name
+    assert_equal Time.parse(start_date), project.start_date
+    assert_equal Time.parse(end_date), project.end_date
+  end
 
 
 
