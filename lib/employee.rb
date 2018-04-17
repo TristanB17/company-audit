6e83dd3e822a926067679d214034b1fba2b7d804
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-17 09:50:54 -0600

    Struggling to instantiate date string as date object.

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,24 @@
+require 'date'
+require './modules/date_handler_module'
+
 class Employee
+  include
+
+
+  attr_reader   :employee_id,
+                :name,
+                :role,
+                :start_date,
+                :end_date
+
+  def initialize(employee_id, name, role, start_date, end_date)
+    @employee_id = employee_id.to_i
+    @name = name
+    @role = role
+    @start_date = DHDate.new(start_date)
+    @end_date = DHDate.new(end_date)
+  end
+
+
+
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -2,11 +2,31 @@ require './test/test_helper'
 require './lib/employee'
 
 class EmployeeTest < Minitest::Test
+
   def test_instantiation
-    employee = Employee.new('./data/employees.csv')
-    assert_instance_of Employee, 
+    employee_id = '5'
+    name = 'Sally Jackson'
+    role = 'Engineer'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+
+    employee = Employee.new(employee_id, name, role, start_date, end_date)
+    assert_instance_of Employee, employee
+    assert_equal 5, employee.employee_id
+    assert_instance_of Date, employee.start_date
   end
 
   def test_attributes
+    employee_id = '5'
+    name = 'Sally Jackson'
+    role = 'Engineer'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    employee = Employee.new(employee_id, name, role, start_date, end_date)
+
+    assert_equal 5, employee.employee_id
   end
+
+
+
 end
